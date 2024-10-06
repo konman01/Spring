64ca1caecaad06a1f3a4a737d1df33dd128e6c57
konman01 <konimanjunatha@gmail.com>
2024-10-06 00:43:28 -0500

    Added Code for Autowire using method call

---
+++ 20241005/example9/src/main/java/com/konman01/main/Example7.java
@@ -0,0 +1,24 @@
+package com.konman01.main;
+
+import com.konman01.beans.Person;
+import com.konman01.beans.Vehicle;
+import com.konman01.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import java.util.Random;
+
+
+public class Example7 {
+
+    public static void main(String[] args) {
+
+       AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+       Person  person = context.getBean(Person.class);
+       Vehicle vehicle = context.getBean(Vehicle.class);
+
+       System.out.println("Person name is:"+person.getName());
+       System.out.println("Vehicle name is:"+vehicle.getName());
+       System.out.println("Person owing the vehicle is:"+person.getVehicle());
+       return;
+    }
+}
